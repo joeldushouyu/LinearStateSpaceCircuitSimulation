95aaa5ffe9dbeb65e8d03f7033387f80a05221b0
joeldushouyu <joeldushouyu@gmail.com>
2025-05-11 13:18:34 -0400

    fix: fix remain issue when switch back to half-bridge llc

--- cppReadCircuitDataLibrary/plot_data.py
+++ cppReadCircuitDataLibrary/plot_data.py
@@ -10,20 +10,20 @@ from visualize_data import plot_csv_ncolumns
 
 def half_brdige_llc_visualize():
     y_labels:dict[str, list[int]]= {
-    "Vmout": [14, 1, 2,14,14,14],
-    "Vp": [9, 2, 7,9,9,9],
-    "Vs1": [11, 3, 8,11,11,11],
-    "Vs2": [13, 4, 9,13,13,13],
-    "VD1": [10, 5, 10,10,10,10],
-    "VD2": [12, 6, 11,12,12,12],
-    "AM_D1": [2, 7, 12,2,2,2],
-    "AM_D2": [3, 8, 13,3,3,3],
-    "AML1": [1, 10, 14,1,1,1],
-    "VC1": [8, 9, 6,8,8,8],
-    "VL1": [7, 14, 5,7,7,7],
-    "Vsw1": [5, 12, 3,5,5,5],
-    "Vsw2": [6, 13, 4,6,6,6],
-    "AMI0": [4, 11, 1,4,4,4]
+    "Vmout": [14, 1, 2,14,14],
+    "Vp": [9, 2, 7,9,9],
+    "Vs1": [11, 3, 8,11,11],
+    "Vs2": [13, 4, 9,13,13],
+    "VD1": [10, 5, 10,10,10],
+    "VD2": [12, 6, 11,12,12],
+    "AM_D1": [2, 7, 12,2,2],
+    "AM_D2": [3, 8, 13,3,3],
+    "AML1": [1, 10, 14,1,1],
+    "VC1": [8, 9, 6,8,8],
+    "VL1": [7, 14, 5,7,7],
+    "Vsw1": [5, 12, 3,5,5],
+    "Vsw2": [6, 13, 4,6,6],
+    "AMI0": [4, 11, 1,4,4]
     }
     sim_20 = "../csv_data/Half-bridge-llcx20.csv"
     host_sim = "hostSim.csv"
@@ -35,10 +35,10 @@ def half_brdige_llc_visualize():
     plec_hil_x20 = "../csv_data/half-bridge-llc-plec-hilx20.csv"
     for lab, index_list in y_labels.items():
         
-        plot_csv_ncolumns( csv_files=[sim_20,plec_x20, plec_hil_x20,host_sim,host_naive,npu_sim],
-                        x_cols= [0,0,0,0,0,0],
+        plot_csv_ncolumns( csv_files=[sim_20,plec_x20, plec_hil_x20,host_sim,npu_sim],
+                        x_cols= [0,0,0,0,0],
                         y_cols= index_list,
-                        labels= ["python", "plec", "plec_hil", "c++", "c++naive", "NPU_sim"],
+                        labels= ["python", "plec", "plec_hil", "c++",  "NPU_sim"],
                         y_label= lab
                         )
         
@@ -79,4 +79,6 @@ def boost_pfc_half_llc_visualize():
                         y_label= lab
                         )    
         
-boost_pfc_half_llc_visualize()
+# boost_pfc_half_llc_visualize()
+
+half_brdige_llc_visualize()
